ac0f9607eb0ff40ec23a2a53aa4b55f710482435
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-01-21 11:34:49 +0100

    specs added

--- specs/collections/RealEstateListSpec.js
+++ specs/collections/RealEstateListSpec.js
@@ -1,5 +1,23 @@
 describe("RealEstateList", function() {
-  it("should TODO", function() { // TODO
-    (true).should.be.true;
-  })
+  var realEstateList;
+
+  beforeEach(function() {
+    var json = [
+      {selected: false},
+      {selected: false},
+      {selected: false}
+    ];
+    realEstateList = new MapSearchApp.Collections.RealEstateList(json);
+  });
+
+  describe("initialize()", function() {
+
+    it("should initialize collection with proper values", function() {
+      var selectedList = realEstateList.pluck("selected");
+      selectedList.should.be.like([false, false, false]);
+    });
+
+  });
+
+  //TODO
 });
