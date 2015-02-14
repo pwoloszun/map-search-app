fc7b8642787b8c5e693a082bcea8bcbcb22052cb
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-02-14 12:04:28 +0100

    fixed pubsub view

--- js/views/MoreResultsPubSubView.js
+++ js/views/MoreResultsPubSubView.js
@@ -1,23 +1,7 @@
 MapSearchApp.Views.MoreResultsPubSubView = Backbone.View.extend({
 
-  template: MapSearchApp.Services.templates.get("#more-results-pub-sub-template"),
-
-  events: {
-    "click .load-more-pub-sub-btn": "loadMoreRealEstates"
-  },
-
   initialize: function(params) {
     this._realEstateRepository = MapSearchApp.Repositories.realEstateRepository;
-  },
-
-  render: function() {
-    var html = this.template();
-    this.$el.html(html);
-  },
-
-  loadMoreRealEstates: function() {
-    this._realEstateRepository.loadMore();
-    MapSearchApp.Services.pubSub.publish("moreResultsPubSub:loaded");
   }
 
 });
